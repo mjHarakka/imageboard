1bf4e80b0dc677edb1f0023a294966834f770bf8
mjHarakka <mj.harakka@gmail.com>
2019-05-16 20:12:49 +0300

    modified connection provider to not show any personal login details

--- src/bean/Provider.java
+++ src/bean/Provider.java
@@ -1,8 +1,8 @@
 package bean;  
   
 public interface Provider {  
-	String DRIVER = "org.mariadb.jdbc.Driver";
-	String CONNECTION_URL = "jdbc:mariadb://localhost:3306/bgg316"; 
-	String USERNAME = "bgg316";  
-	String PASSWORD = "reRY5T33r";  
+	String DRIVER = "example.jdbc.Driver";
+	String CONNECTION_URL = "jdbc:example://localhost:3306/database"; 
+	String USERNAME = "username";  
+	String PASSWORD = "password";  
 }
